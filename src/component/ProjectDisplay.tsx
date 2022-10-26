5019bd6958d6bc2fb8d3db4b3475a321d9fa9d94
Macie Bey <maciebey@gmail.com>
2022-10-26 16:39:33 -0500

    Init sticky header

--- src/component/ProjectDisplay.tsx
+++ src/component/ProjectDisplay.tsx
@@ -3,13 +3,33 @@ import { Link } from "react-router-dom";
 import './ProjectDisplay.css';
 // types
 import { project } from '../config'
+import { useLayoutEffect, useRef } from "react";
 
 type ProjectDisplayTypes = {
   projectData: project[]
 }
 const ProjectDisplay = ({projectData}:ProjectDisplayTypes) => {
+  const stickyRef = useRef<HTMLDivElement>(null)
+  useLayoutEffect(() => {
+    let fixedTop = stickyRef.current!.offsetTop
+    const fixedHeader = () => {
+      if (window.pageYOffset > fixedTop) {
+        stickyRef.current!.classList.add('stuck')
+      } else {
+        stickyRef.current!.classList.remove('stuck')
+      }
+    }
+    window.addEventListener('scroll', fixedHeader)
+  }, [])
+
   return (
+    <>
+    <div ref={stickyRef} className='project-header'>
+      <h2 className="vis-on-stuck">Person Lastname&nbsp;-&nbsp;</h2>
+      <h2>Public Portfolio</h2>
+    </div>
     <div className='project-container'>
+      <div>
       {projectData.map((pData)=> <div className='project-card' key={pData.name}>
         <div className='project-card-inner'>
           <div className='project-card-preview'>
@@ -20,7 +40,9 @@ const ProjectDisplay = ({projectData}:ProjectDisplayTypes) => {
           <Link to={`/${pData.name}`}>{pData.name}</Link>
         </div>
       </div>)}
+      </div>
     </div>
+    </>
   )
 }
 
