ba5318eee3c358acdc6b384b88612b5b0a08fb6e
Macie Bey <maciebey@gmail.com>
2022-11-02 19:01:04 -0500

    Updated data with real info

--- src/config/data.ts
+++ src/config/data.ts
@@ -2,7 +2,11 @@
 export type profile = {
   name: string,
   title: string,
-  deets: string[]
+  deets: string[],
+  links: {
+    github: string,
+    linkedin: string
+  }
 }
 export const profileData: profile = {
   name: "Macie Bey",
@@ -11,7 +15,11 @@ export const profileData: profile = {
     "Oh wow",
     "Truely a beautiful site",
     "Look at that rotating gradient wow"
-  ]
+  ],
+  links: {
+    github: "https://github.com/maciebey/",
+    linkedin: "https://www.linkedin.com/in/maciebey"
+  }
 }
 
 /* Project Types and Data */
@@ -25,25 +33,67 @@ export type project = {
   name: string,
   image: string,
   tags: tags[],
-  description: string
+  description: string,
+  ghLinks: {
+    text: string,
+    url: string
+  }[],
+  liveLinks?: {
+    text: string,
+    url: string
+  }[],
 }
 export const projectData: project[] = [
   {
-    name: "One",
+    name: "Great Wave",
     image: "wave.png",
     tags: [tags.TypeScript, tags.React, tags.Redux],
-    description: "big big yoshi"
+    description: "Interactive SVG manipulator.",
+    ghLinks: [
+      {
+        text: "GitHub",
+        url: "https://github.com/maciebey/great-wave"
+      }
+    ],
+    liveLinks: [
+      {
+        text: "gw.mbdv.io",
+        url: "http://gw.mbdv.io/"
+      }
+    ]
   },
   {
-    name: "Two",
+    name: "Unnamed Sound App",
     image: "mix.png",
     tags: [tags.TypeScript, tags.React, tags.Redux, tags.Heroku],
-    description: "bigger big big yoshi"
+    description: "Personal application to mix YT video w/ audio files. Future state pivoting to full white/background noise generator.",
+    ghLinks: [
+      {
+        text: "GitHub (UI)",
+        url: "https://github.com/maciebey/equalizer"
+      },
+      {
+        text: "GitHub (API)",
+        url: "https://github.com/maciebey/equalizer-api"
+      }
+    ],
+    liveLinks: [
+      {
+        text: "eq.mbdv.io",
+        url: "http://eq.mbdv.io/"
+      }
+    ]
   },
   {
-    name: "Three",
+    name: "My Importfolio",
     image: "portfolio.png",
-    tags: [],
-    description: "biggest yoshur"
+    tags: [tags.TypeScript, tags.React],
+    description: "You're here now! My portfolio & landing page for mbdv.io.",
+    ghLinks: [
+      {
+        text: "GitHub",
+        url: "https://github.com/maciebey/mb-portfolio"
+      }
+    ]
   }
 ]
