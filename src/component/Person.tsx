03172b7b9ff94e35b7efe34dd7ef0ac750de45a1
Macie Bey <maciebey@gmail.com>
2022-11-02 19:03:50 -0500

    Name/title from data, minor css adjustment

--- src/component/Person.tsx
+++ src/component/Person.tsx
@@ -8,10 +8,10 @@ const Person = ({profileData}:PersonProps) => {
   return (
     <div className='person-container'>
       <div className='person-image'>
-        <img src='/img/prof.jpg' alt='person lastname' />
+        <img src='/img/prof.jpg' alt={`${profileData.name}`} />
       </div>
       <div className='person-details'>
-        <h1>Person Lastname - Job Title</h1>
+        <h1>{profileData.name} - {profileData.title}</h1>
         {profileData.deets.map((deet) => <p>{deet}</p>)}
       </div>
     </div>
